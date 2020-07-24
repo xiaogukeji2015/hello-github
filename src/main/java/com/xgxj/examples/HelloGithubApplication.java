fdd110d318b734371c0ed6882066322a190e0d0d
manheiba <manheiba@163.com>
2020-07-24 16:27:12 +0800

    added project file

---
+++ src/main/java/com/xgxj/examples/HelloGithubApplication.java
@@ -0,0 +1,15 @@
+package com.xgxj.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class HelloGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGithubApplication.class, args);
+	}
+
+}
